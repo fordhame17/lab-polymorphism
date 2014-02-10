4ed6eff876dcb51bc2ffff613ff97f264880e3e8
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-10 17:59:11 -0600

    Add first two classes for the lab.

---
+++ src/taojava/labs/polymorphism/MathExpt.java
@@ -0,0 +1,28 @@
+package taojava.labs.polymorphism;
+
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+/**
+ * A few simple experiments with our utilities.
+ */
+public class MathExpt
+{
+  public static void main(String[] args)
+      throws Exception
+  {
+    PrintWriter pen = new PrintWriter(System.out, true);
+
+    // Print some square roots.
+    for (int i = 2; i < 10; i++)
+      {
+        double root = MathUtils.squareRoot(i);
+        pen.println("The square root of " + i + " seems to be " + root);
+        pen.println(root + "^2 = " (root*root));
+      } // for
+ 
+    // We're done.  Clean up.
+    pen.close();
+  } // main(String[])
+} // class MathExpt
---
+++ src/taojava/labs/polymorphism/MathUtils.java
@@ -0,0 +1,37 @@
+package taojava.labs.polymorphism;
+
+/**
+ * A collection of simple math utilities.
+ */
+public class MathUtils
+{
+/**
+ * A collection of Math utilities.
+ *
+ * @author Samuel A. Rebelsky
+ * @version 1.0 of September 2005
+ */
+public class Mathematician
+{
+  /**
+   * Compute an approximate square root of num.
+   *
+   * @pre num > 1.0
+   * @post the square of the result is within .01 of num.
+   */
+  public static double squareRoot(Number num)
+  {
+    double n = num.doubleValue();
+    double lower = 1.0;
+    double upper = n;
+    while (upper-lower > 0.01) 
+      {
+        double mid = upper/2.0 + lower/2.0;  
+        if (mid*mid < n)
+          lower = mid;
+        else
+          upper = mid;
+      } // while
+    return (upper+lower)/2.0;
+  } // squareRoot(Number)
+} // class MathUtils
