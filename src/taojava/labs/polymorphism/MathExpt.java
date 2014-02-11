179e9f6e05b47da49d43157104c6d70e6a49829c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-10 20:35:43 -0600

    Minor update to comment in MathExpt.

--- src/taojava/labs/polymorphism/MathExpt.java
+++ src/taojava/labs/polymorphism/MathExpt.java
@@ -1,8 +1,6 @@
 package taojava.labs.polymorphism;
 
 import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 
 /**
  * A few simple experiments with our utilities.
@@ -20,7 +18,7 @@ public class MathExpt
         double root = MathUtils.squareRoot(i);
         pen.println("The square root of " + i + " seems to be " + root);
         pen.println(root + "^2 = " + (root*root));
-      } // for
+      } // for i
  
     // We're done.  Clean up.
     pen.close();
